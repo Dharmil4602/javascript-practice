a30f87f91bad50c3ee531ab8739f75d5d458acf2
Dharmil Shah <dharmilshah476@gmail.com>
2021-08-17 01:06:40 +0530

    Square Root

---
+++ sqaureRoot.js
@@ -0,0 +1,6 @@
+console.log(`Sqaure Root`);
+console.log(`Sqaure Root Of 36 is: ${Math.sqrt(36)}`);
+console.log(`Sqaure Root Of 54 is: ${Math.sqrt(54)}`);
+console.log(`Sqaure Root Of 100 is: ${Math.sqrt(100)}`);
+
+// Similar Way You Can Also Find Various SquareRoots.
