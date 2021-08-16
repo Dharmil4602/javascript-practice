72b7b33d013bdce6efdea67940c36b46fa302add
Dharmil Shah <dharmilshah476@gmail.com>
2021-08-17 00:30:19 +0530

    Hello World

---
+++ helloworld.js
@@ -0,0 +1,10 @@
+// Using Console
+console.log(`Hello World..!!!`);
+
+// Using Alert
+setTimeout(() => {
+    alert(`Hello World.!!`);
+    }, 1000);
+
+//Using Document.write
+document.write(`Hey There`);
