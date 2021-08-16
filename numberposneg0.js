17815b0da74855d5f320f6b42bd55425a6fdc0f6
Dharmil Shah <dharmilshah476@gmail.com>
2021-08-17 02:32:35 +0530

    Positive Negative 0

---
+++ numberposneg0.js
@@ -0,0 +1,15 @@
+let n = parseInt(prompt(`Enter The Number: `));
+
+if (n == 0) {
+    console.log(`Entered Number Is 0`);
+}
+
+else if(n > 0)
+{
+    console.log(`Entered Number Is Positive`);
+}
+
+else
+{
+    console.log(`Number Is Negative`);
+}
