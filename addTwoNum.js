13a1e2bc127ba2215e2836384e95f56d278a5170
Dharmil Shah <dharmilshah476@gmail.com>
2021-08-17 01:00:05 +0530

    Adding Two Number Modified

---
+++ addTwoNum.js
@@ -0,0 +1,7 @@
+console.log(`Hey`);
+function sum(a,b)
+{
+    return a + b;
+}
+
+// To add two numbers, open this program in your browser and then press f12, go to console and type sum and then () and insert the values you want to add.
