b88235b42c188c8de5ee8af93b0600bf430d74da
Dharmil Shah <dharmilshah476@gmail.com>
2021-08-18 00:00:21 +0530

    Print Prime Numbers In Given Interval

--- printPrimeInInterval.js
+++ printPrimeInInterval.js
@@ -2,7 +2,7 @@ let start,end,num;
 start = parseInt(prompt(`Enter The Starting Number Of Interval: `));
 end = parseInt(prompt(`Enter The Ending Number Of Interval: `));
 
-for(let i = start; i<end; i++)
+for(let i = start; i<=end; i++)
 {
     num = 1;
     for(let j = 2; j<=i/2; j++)
