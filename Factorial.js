a10e7b99df42673ba6b927e474f9754cf79da436
Dharmil Shah <dharmilshah476@gmail.com>
2021-09-10 01:15:05 +0530

    Added Negative And 0 Factorial Options

--- Factorial.js
+++ Factorial.js
@@ -1,8 +1,22 @@
 let fact = 1, n;
 
 n = parseInt(prompt(`Enter The Number You Want Factorial Upto: `));
+
+if(n === 0)
+{
+    document.write(`<h2> Factorial Of 0 Is 1 </h2>`);
+}
+
+else if(n < 0)
+{
+document.write(`<h2> Error! You Entered Negative Number </h2>`);
+}
+
+else
+{
 for (let i = 1; i <= n; i++)
 {
     fact = fact * i;
 }
 document.write(`<h3> Factorial Of ${n} Is: ${fact} </h3>`);
+}
